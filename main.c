1949e6e9ef6cdd7347a84210074b6bb2c1b7b327
Mavvrikk <116310955+Mavvrikk@users.noreply.github.com>
2023-02-21 22:37:17 -0600

    Added Process Table
    Added Process table to main and updated functions to include it

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ struct PCB {
     pid_t pid; // PID of this child
     int startSeconds; // time this was forked
     int startNano; // time this was forked (nano seconds)
-}
+};
 
 void helpFunction()
 {
@@ -22,7 +22,7 @@ void helpFunction()
     printf("where -h displays this help function and terminates, proc is the number of processes you want to run, simu is the number of simultaneous");
     printf("processes that run and timeLimit is approximately the time limit each child process runs.");
 }
-int forker(int totaltoLaunch, int simulLimit, char* timeLimit, int totalLaunched)
+int forker(int totaltoLaunch, int simulLimit, int timeLimit, int totalLaunched,int* stillChildrentoLaunch,struct PCB *processTable)
 {
     pid_t pid;
     if(totalLaunched==simulLimit){
@@ -36,42 +36,40 @@ int forker(int totaltoLaunch, int simulLimit, char* timeLimit, int totalLaunched
         }
         else if (pid == 0)
         {
-        /*char* args[] = {"./worker", timeLimit,0};
-        execlp(args[0],args[0],args[1],args[2]);*/
-        
-        /*    CONTENTS OF WORKER FILE
-        int shmkey = 112369;
-        int main (int argc, char** argv){
-        int* shmPtr1;
-   
-        int shmid = shmget(shmkey,2*sizeof(int),0777|IPC_CREAT); // create shared memory
-        if(shmid==-1)
-        {
-        printf("ERROR IN SHMGET\n");
+            processTable[totalLaunched].occupied = 1;
+            processTable[totalLaunched].pid = pid;
+            processTable[totalLaunched].startSeconds = 0;
+            processTable[totalLaunched].startNano = 1;
+            
+        printf("I am a child and supposed to run %d seconds\n", timeLimit);
         exit(0);
-        }
-        shmPtr1 = (int*)(shmat(shmid,0,0));
-        */
-        int runTime;
-        runTime = timeLimit; // atoi(argv[1])
-        printf("I am a child and supposed to run %d seconds\n", runTime);
-}
 
-        
         }
         else if(pid > 0)
         {
-            forker(totaltoLaunch - 1, simulLimit, timeLimit, totalLaunched + 1);
+            forker(totaltoLaunch - 1, simulLimit, timeLimit, totalLaunched + 1, stillChildrentoLaunch, processTable);
         }
     }
-    else
+    else{
+        *stillChildrentoLaunch = 1;
         return (0);
+    }
+}
+
+void printStruct(struct PCB *processTable, int size){
+    printf("Process Table: \n");
+    printf("ENTRY  OCCUPIED  PID  STARTS  STARTN\n");
+    for (int i = 0; i<size; i++){
+        printf("%d        %d        %d        %d        %d\n", i,processTable->occupied,processTable->pid,processTable->startSeconds,processTable->startNano);
+    }
 }
 
 char *x = NULL;
 char *y = NULL;
-char *timeLimit = NULL;
- 
+char *z = NULL;
+struct PCB processTable[20];
+
+
 int main(int argc, char** argv)
 {
     int option;
@@ -87,7 +85,7 @@ int main(int argc, char** argv)
             y = optarg;
             break;
             case 't': 
-            timeLimit = optarg;
+            z = optarg;
             break;
         }
     }
@@ -96,8 +94,11 @@ int main(int argc, char** argv)
    int totalLaunched=0; // int to count total children launched
    totaltoLaunch = atoi(x); // casting the argv to ints
    simulLimit = atoi(y);
+   int timeLimit = atoi(z);
    int status;
    int* shmPtr1;
+   int* stillChildrentoLaunch = 0; // bool to know if children need to be launched
+
    
   int shmid = shmget(shmkey,2*sizeof(int),0777|IPC_CREAT); // create shared memory
   if(shmid==-1)
@@ -106,20 +107,20 @@ int main(int argc, char** argv)
       exit(0);
   }
    shmPtr1 = (int*)(shmat(shmid,0,0));
-   *shmPtr1 = 125;
-   
-   int exCess = forker(totaltoLaunch,simulLimit,timeLimit,totalLaunched);
-   pid_t pid;
-   pid = wait(&status);
-    if (exCess>0)
-    for(exCess;exCess>0;exCess--){
-        forker(1,1,timeLimit,totalLaunched);
+   *shmPtr1 = 0;
+       *shmPtr1++;
+       int exCess = forker(totaltoLaunch,simulLimit,timeLimit,totalLaunched,stillChildrentoLaunch,processTable);
+       printf("%d\n", exCess);
+       if (exCess>0)
+       for(exCess;exCess>0;exCess--){
+           forker(1,1,timeLimit,totalLaunched,stillChildrentoLaunch,processTable);
     }
+    pid_t pid = wait(&status);
+    printStruct(processTable,20);
     shmdt(shmPtr1); // detach shared memory
     shmctl(shmid,IPC_RMID,NULL);
     return(0);
 }
-
 
 
 
